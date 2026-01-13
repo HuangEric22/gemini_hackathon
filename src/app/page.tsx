abdfc8d2d9806ec1fe5599e6fe6b6520c29e0496
HuangEric22 <huangeric50@gmail.com>
2026-01-12 17:50:45 -0800

    removed empty component and adjust sizing on panels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,17 +23,14 @@ export default function Home() {
   };
 
   return (
-    // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    //   <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-    //     <SearchCard onSearch={handleSearch} isLoading={isSearching} />
-    //   </main>
-    // </div>
     <div className="flex h-screen w-full overflow-hidden">
       <Group orientation='horizontal' className='w-full'>
 
-      <Panel><DiscoveryFeed onSearch={handleSearch} isSearching={isSearching} cityName='Los Angeles'/></Panel>
+      <Panel minSize="10%" maxSize="20%" defaultSize="10%"><Sidebar/></Panel>
       <ResizeSeparator/>
-      <Panel><MapArea/></Panel>
+      <Panel minSize="30%" defaultSize="60%"><DiscoveryFeed onSearch={handleSearch} isSearching={isSearching} cityName='Los Angeles'/></Panel>
+      <ResizeSeparator/>
+      <Panel minSize="20%" ><MapArea/></Panel>
 
     </Group>
     </div>
