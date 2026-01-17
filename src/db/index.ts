bfb86c50c432017a996a1d6dc1cea2a9c75c987b
Puttaranun <puttaranun_jinny@hotmail.com>
2026-01-16 21:50:12 -0800

    add mytrip backend logic

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
-export const db = drizzle(process.env.DB_FILE_NAME!);
+import { createClient } from '@libsql/client'; // Recommended for LibSQL
+import * as schema from './schema';
+
+// Create the client explicitly
+const client = createClient({ 
+  url: process.env.DB_FILE_NAME! 
+});
+
+// Pass the schema in the config object
+export const db = drizzle(client, { schema });
